428379e07763729d97f50b2a351c9162390b5b4c
K-KY <kyuyoungk@naver.com>
2025-04-19 11:40:02 +0900

    Feat : 웹소켓 연결 추가
    - 문제점
     - 웹소켓을 한번 종료 했을 때 다시 연결되는 현상

--- app/model/char_data_collector/chart_collector.py
+++ app/model/char_data_collector/chart_collector.py
@@ -1,7 +1,13 @@
 import requests
 import time
+import asyncio
+import websockets
+import json
+import ssl
+
 
 base_url = "https://www.binance.com/fapi/v1/continuousKlines?limit={}&pair={}&contractType=PERPETUAL&interval={}&startTime={}"
+BINANCE_WS_URL = "wss://fstream.binance.com/stream?streams={}@continuousKline_{}"
 
 def collect_chart(limit, coin, interval, time_stamp) :
     url = base_url.format(limit, coin, interval, time_stamp)
@@ -10,6 +16,20 @@ def collect_chart(limit, coin, interval, time_stamp) :
 
     return webpage.json()
 
-gettimestamp = int((time.time() - 60 * 60 * 24 * 120) * 1000)  # 120일 전의 타임스탬프
-data = collect_chart(100, "ETHUSDT", "1d", gettimestamp)
-print(data)
+async def listen_binance_kline(coin, interval):
+    ssl_context = ssl._create_unverified_context()
+    url = BINANCE_WS_URL.format(coin, interval)
+    async with websockets.connect(url, ssl=ssl_context) as websocket:
+        print("WebSocket 연결됨!")
+        while True:
+            data = await websocket.recv()
+            parsed = json.loads(data)
+            kline = parsed['data']['k']
+            print(parsed)
+
+# gettimestamp = int((time.time() - 60 * 60 * 24 * 120) * 1000)  # 120일 전의 타임스탬프
+# data = collect_chart(100, "ETHUSDT", "1d", gettimestamp)
+# print(data)
+#
+# if __name__ == "__main__":
+#     asyncio.run(listen_binance_kline("btcusdt_perpetual", "1m"))
