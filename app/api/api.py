97563e93a795c280c593803ad31a20c38467ff93
K-KY <kyuyoungk@naver.com>
2025-04-21 00:43:02 +0900

    Refactor : 코드 정리

--- app/api/api.py
+++ app/api/api.py
@@ -1,16 +1,12 @@
 import subprocess
-import threading
 import uvicorn
 
-from app.model.char_data_collector.chart_collector import listen_binance_kline
-from app.model.char_data_collector.chart_collector import run_listen_kline
+from app.model.char_data_collector.chart_collector import run_monitor_chart, get_all_workers
 
 from fastapi import FastAPI
 
 from app.api_test import api_test
 
-stop_event = threading.Event()
-threads = {}
 
 app = FastAPI()
 app.include_router(api_test.router, prefix='/api')
@@ -20,34 +16,16 @@ app.include_router(api_test.router, prefix='/api')
 def test():
     return "OK"
 
-# 단일 차트 웹소켓
-@app.get("/connect-wss")
-async def get_chart(coin, interval):
-    await listen_binance_kline(coin, interval)
-    return {"message": "Started websocket"}
-
-
 # 차트 웹소켓 스레드
 @app.get("/start-task")
 async def start_background_chart_monitoring(coin, interval):
-    name = "thread-worker " + str(len(threads) + 1) + ",coin = " + coin + ",interval = " + interval
-    thread = threading.Thread(target=run_listen_kline, args=(coin, interval),
-                              name=name, daemon=True)
-    threads[name] = thread
-    thread.start()
+    run_monitor_chart(coin, interval)
     return {"status": "작업이 백그라운드에서 시작되었습니다."}
 
-# 실행중인 스레드 목록
+#실행중인 스레드 목록
 @app.get("/workers")
 def workers():
-    return [
-        {
-            "name": name,
-            "alive": t.is_alive(),
-            "ident": t.ident
-        }
-        for name, t in threads.items()
-    ]
+    return get_all_workers()
 
 
 if __name__ == "__main__":
