e6dde51d34bde709f03a042d4e1763056ad52ba1
K-KY <kyuyoungk@naver.com>
2025-04-23 23:04:27 +0900

    Feat : 쓰레드 개별 종료 기능 추가
    - 종료 요청후 종료가 된 상태에서 바로 다시 요청을 보내면 종료되었다는 메세지가 아니라 요청됨으로 나옴

--- app/model/char_data_collector/chart_collector.py
+++ app/model/char_data_collector/chart_collector.py
@@ -9,33 +9,38 @@ import threading
 BASE_URL = "https://www.binance.com/fapi/v1/continuousKlines"
 BINANCE_WS_URL = "wss://fstream.binance.com/stream?streams={}@continuousKline_{}"
 
-stop_event = threading.Event()
+stop_event = {}
 threads = {}
+
+# 개별 쓰레드를 종료시킬 방법 생각
 
 #쓰레드 생성, 작업 할당
 def run_monitor_chart(coin: str, interval: str):
-    name = "thread-worker " + str(len(threads) + 1) + ",coin = " + coin + ",interval = " + interval
+    name = "thread-worker_" + str(len(threads) + 1) + "__coin__" + coin + "__interval__" + interval
+
+    event = threading.Event()
     thread = threading.Thread(
         target=__run_listen_kline,
-        args=(coin, interval),
+        args=(coin, interval, name),
         name=name,
         daemon=True
     )
     threads[name] = thread
+    stop_event[name] = event
     thread.start()
 
 #쓰레드가 수행할 작업
-def __run_listen_kline(coin: str, interval: str):
+def __run_listen_kline(coin: str, interval: str, name):
     loop = runner()
-    loop.run_until_complete(listen_binance_kline(coin, interval))
+    loop.run_until_complete(listen_binance_kline(coin, interval, name))
 
 # binance 웹소켓 연결, 여기에 지표 추가
-async def listen_binance_kline(coin: str, interval: str):
+async def listen_binance_kline(coin: str, interval: str, name:str):
     ssl_context = ssl._create_unverified_context()
     url = BINANCE_WS_URL.format(coin, interval)
     async with websockets.connect(url, ssl=ssl_context) as websocket:
         print(f"WebSocket 연결됨! ({coin}, {interval})")
-        while True:
+        while not stop_event[name].is_set() :
             data = await websocket.recv()
             parsed = json.loads(data)
             kline = parsed['data']['k']
@@ -66,6 +71,16 @@ def get_all_workers():
         }
         for name, t in threads.items()
     ]
+
+## 쓰레드 개별 종료 로직 개선의 여지가 있음
+def stop_thread(thread_name:str):
+    if thread_name in stop_event and not threads[thread_name].is_alive():
+        return thread_name + " 이미 종료됨"
+    if thread_name in stop_event:
+        stop_event[thread_name].set()  # 쓰레드 종료 신호
+        print(f"[{thread_name}] 종료 요청됨")
+        return thread_name + " 종료 요청 완료"
+    return thread_name + " 찾을 수 없음"
 
 # 이건 다른 파일로 분리 되어도 될거같음
 def runner():
